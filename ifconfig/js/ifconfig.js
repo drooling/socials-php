869dae0fde256b51d9d392c2544a0accec18eece
Surtains <97707885+drooling@users.noreply.github.com>
2022-05-22 21:22:51 +0000

    Update ifconfig.js

--- ifconfig/js/ifconfig.js
+++ ifconfig/js/ifconfig.js
@@ -1,12 +1,12 @@
 function load_data() {
     let session = new XMLHttpRequest();
-    session.open("GET", "https://ip-api.com/json/".replace('https', 'http'));
+    session.open("GET", "https://ipapi.co/json/");
     session.send();
 
     session.onload = function () {
         const jobj = JSON.parse(session.responseText);
         document.getElementById("ipv4-address").textContent = jobj.query;
-        document.getElementById("location").textContent = `Location: ${jobj.city}, ${jobj.regionName}, ${jobj.country}`;
-        document.getElementById("provider").textContent = `Provider: ${jobj.isp}`;
+        document.getElementById("location").textContent = `Location: ${jobj.city}, ${jobj.region}, ${jobj.country}`;
+        document.getElementById("provider").textContent = `Provider: ${jobj.org}`;
     };
 }
