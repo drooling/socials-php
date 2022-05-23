aef36195243f402116779272f727bcf429fbc5b0
Surtains <97707885+drooling@users.noreply.github.com>
2022-05-23 18:40:14 +0000

    Update ifconfig.js

--- ifconfig/js/ifconfig.js
+++ ifconfig/js/ifconfig.js
@@ -1,12 +1,12 @@
 function load_data() {
     let session = new XMLHttpRequest();
-    session.open("GET", "https://ipapi.co/json/");
+    session.open("GET", "https://api.techniknews.net/ipgeo/");
     session.send();
 
     session.onload = function () {
         const jobj = JSON.parse(session.responseText);
         document.getElementById("ipv4-address").textContent = jobj.ip;
-        document.getElementById("location").textContent = `Location: ${jobj.city}, ${jobj.region}, ${jobj.country}`;
-        document.getElementById("provider").textContent = `Provider: ${jobj.org}`;
+        document.getElementById("location").textContent = `Location: ${jobj.city}, ${jobj.regionName}, ${jobj.country}`;
+        document.getElementById("provider").textContent = `Provider: ${jobj.isp}`;
     };
 }
